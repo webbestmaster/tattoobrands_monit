bf4cb0157ee4c5d6764da130c8de09a138592e08
Dmitry Turovtsov <web.best.master@gmail.com>
2017-10-16 22:10:47 +0300

    refactor for production

--- index.js
+++ index.js
@@ -7,32 +7,52 @@ const nodemailer = require('nodemailer');
 const smtpTransport = require('nodemailer-smtp-transport');
 
 const {CheckMaster} = require('url-master');
+const checkPeriod = 10e3; // 10 seconds
+
+const guard = {
+    counter: 0,
+    maxCount: 60 * 60 * 1e3 / checkPeriod,
+    tryRun() {
+        if (guard.counter === 0) {
+            guard.counter = 1;
+            return true;
+        }
+
+        guard.counter += 1;
+
+        if (guard.counter >= guard.maxCount) {
+            guard.counter = 0;
+        }
+
+        return false;
+    }
+};
+
+function sendMail(errorUrls) {
+    const mailOptions = {
+        from: ADMIN_EMAIL,
+        to: ADMIN_EMAIL, // eslint-disable-line id-length
+        subject: 'Aaaaaaaaaight! TattooBrands is down!',
+        html: errorUrls.statuses.map(status => '<p>' + status + '</p>').join('')
+    };
+
+    const transporter = nodemailer.createTransport(smtpTransport({
+        service: 'gmail',
+        auth: {
+            user: ADMIN_EMAIL,
+            pass: ADMIN_PASS
+        }
+    }));
+
+    transporter.sendMail(mailOptions, mailErr => mailErr ?
+        console.error(mailErr) :
+        console.log('Email sent to:', ADMIN_EMAIL));
+}
 
 const checkMaster = new CheckMaster({
-    period: 60 * 10 * 1e3, // every 10 minutes
-    urls: ['http://tattoobrands.by1', 'http://tattoobrands.by:30001', 'http://tattoobrands.by:3000/category/root'],
-    onError: urlErr => {
-        const mailOptions = {
-            from: ADMIN_EMAIL,
-            to: ADMIN_EMAIL, // eslint-disable-line id-length
-            subject: 'Aaaaaaaaaight! TattooBrands is down!',
-            html: urlErr.statuses.map(status => '<p>' + status + '</p>').join('')
-        };
-
-        const transporter = nodemailer.createTransport(smtpTransport({
-            service: 'gmail',
-            auth: {
-                user: ADMIN_EMAIL,
-                pass: ADMIN_PASS
-            }
-        }));
-
-        transporter.sendMail(mailOptions, mailErr => mailErr ?
-            console.error(mailErr) :
-            console.log('Email sent to:', ADMIN_EMAIL));
-    }
+    period: checkPeriod, // every 10 seconds
+    urls: ['http://tattoobrands.by', 'http://tattoobrands.by:3000', 'http://tattoobrands.by:3000/category/root'],
+    onError: errorUrls => guard.tryRun() && sendMail(errorUrls)
 });
 
 checkMaster.run();
-
-
